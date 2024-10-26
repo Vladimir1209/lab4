3d5b3ed553ffb654a636ca60eea29841978b36ba
Vladimir1209 <108366469+Vladimir1209@users.noreply.github.com>
2024-10-26 14:15:04 +0300

    Update 5.cpp

--- 5.cpp
+++ 5.cpp
@@ -4,4 +4,32 @@
 #include <algorithm>
 using namespace std;
 
-bool remove_first_negative_element
+bool remove_first_negative_element( vector<int>& vec, int& removed_element){
+    bool a = false;
+    removed_element = 0;
+    for(int i = 0; i< vec.size();++i){
+        auto t = vec.begin();
+        if(vec[i]<0){
+             removed_element = vec[i];
+             vec.erase(t + i);
+             a = true;
+             break;
+        }
+        
+    }
+    
+    return a;
+
+};
+
+int main(){
+    int s;
+    vector<int> very = {5, 12, -47, -5};
+    cout<<remove_first_negative_element(very,s)<<endl;
+    
+    for(int i = 0; i<very.size();++i){
+        
+        cout<<very[i]<<endl;
+    }
+    cout<<"удаленный элемент: "<<s<<endl;
+}
